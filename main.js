09b44f8acd8c0aa38636cf9d8aa3fa43509a532f
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-09 15:03:19 -0400

    finished all steps

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+function appendUl(element){
+    const firstUl = document.querySelector('ul')
+    firstUl.appendChild(element)
+}
 
+function appendLorem(element){
+    const childOfLorem = document.querySelector('#lorem')
+    childOfLorem.appendChild(element)
+}
+
+
+function newLi(str){
+    const newTextLocation = document.querySelector('li')
+    newTextLocation.innerText = str
+
+    return newTextLocation
+}
+
+function newImage(str,img){
+    const newImage1 = document.querySelector('img')
+        newImage1.src = str
+}
+
+
+function sameClassElements(element1, element2){
+    const something1 = document.querySelector(element1)
+    const something2 = document.querySelector(element2)
+
+    something2.ClassName = something1.ClassName
+}
+
+
+function newElementSameTag(name, id){
+    const newElement1 = document.createElement(name)
+    newElement1.idName= id
+}
+
+function colorId(color, iD){
+    const newColor = style.color = 
+}
